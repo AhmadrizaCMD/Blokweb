c78a22d96a57b09aed024cf21f7a15e8e751e368
AhmadrizaCMD <150698950+AhmadrizaCMD@users.noreply.github.com>
2024-01-24 11:39:50 +0100

    Add files via upload

---
+++ scripts/script2.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+console.log("hi");
+
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
